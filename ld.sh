595d8945045172168f272f1ee40ceb21a3119997
oharring <88949304+oharring@users.noreply.github.com>
2022-07-19 15:35:37 -0400

    Update ld.sh

--- ld.sh
+++ ld.sh
@@ -1,1 +1,3 @@
+#emerald2windowldcounts.pl from https://github.com/owensgl/reformat
+
 bcftools view -s SAMPLE_LIST -r CHROM INPUT_VCF.vcf.gz | vcftools --vcf - --maf 0.05 --thin 1000 -c --geno-r2 --max-missing-count 0 | perl emerald2windowldcounts_osh.pl | gzip > OUTPUT.ld.txt.gz
